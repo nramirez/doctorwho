66ba38d31b87c4de9753fdc50865e892f65f56a2
Nazaret Ramirez <ram.nazaret@gmail.com>
2021-03-24 00:39:31 -0500

    Validacion de codigo, Manejando multiples formularios

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,8 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   var _emailForm = GlobalKey<FormState>();
+  var _codeForm = GlobalKey<FormState>();
+  var authCode = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -34,35 +36,82 @@ class _LoginPageState extends State<LoginPage> {
       appBar: AppBar(
         title: Text("Dr. Soler"),
       ),
-      body: Form(
-        key: _emailForm,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                  width: 300,
-                  child: TextFormField(
-                    validator: (value) => value.isEmpty
-                        ? "Correo Requerido"
-                        : EmailValidator.validate(value)
-                            ? null
-                            : "Correo Incorrecto",
-                    decoration: InputDecoration(labelText: "Email"),
-                  )),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextButton(
-                    onPressed: () {
-                      if (_emailForm.currentState.validate()) {
-                        print("Vamonos!!!");
-                      }
-                    },
-                    child: Text("Entrar")),
-              )
-            ],
-          ),
+      body: authCode > 0 ? codeForm() : emailForm(),
+    );
+  }
+
+  Form emailForm() {
+    return Form(
+      key: _emailForm,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+                width: 300,
+                child: TextFormField(
+                  validator: (value) => value.isEmpty
+                      ? "Correo Requerido"
+                      : EmailValidator.validate(value)
+                          ? null
+                          : "Correo Incorrecto",
+                  decoration: InputDecoration(labelText: "Email"),
+                )),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                  onPressed: () {
+                    if (_emailForm.currentState.validate()) {
+                      setState(() {
+                        authCode = 3525;
+                      });
+                    }
+                  },
+                  child: Text("Entrar")),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Form codeForm() {
+    return Form(
+      key: _codeForm,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Revisa tu correo",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+                width: 100,
+                child: TextFormField(
+                  validator: (value) =>
+                      authCode > 0 && int.tryParse(value) == authCode
+                          ? null
+                          : "incorrecto",
+                  keyboardType: TextInputType.number,
+                  maxLength: 4,
+                  decoration: InputDecoration(
+                    labelText: "código",
+                  ),
+                )),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                  onPressed: () {
+                    if (_codeForm.currentState.validate()) {
+                      print("Vamonos!!!");
+                    }
+                  },
+                  child: Text("Entrar")),
+            )
+          ],
         ),
       ),
     );
