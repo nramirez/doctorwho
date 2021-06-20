f04d87aee69dfe017ca6cab16b069507ae527a49
Nazaret Ramirez <ram.nazaret@gmail.com>
2021-06-20 17:26:14 -0500

    search in flutter

--- lib/screens/admin/patients/list.dart
+++ lib/screens/admin/patients/list.dart
@@ -13,61 +13,99 @@ class PatientsPage extends StatefulWidget {
 }
 
 class _PatientsPageState extends State<PatientsPage> {
-  ProfileService _profileService = ProfileService();
+  TextEditingController searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: adminNavbar(context),
-        body: FutureBuilder(
-          future: _profileService.getAll(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState != ConnectionState.done) {
-              return CircularProgressIndicator();
-            }
+        body: Center(
+          child: SizedBox(
+            width: 300,
+            child: Column(
+              children: [
+                TextField(
+                  controller: searchController,
+                  onChanged: (value) {
+                    setState(() {});
+                  },
+                  decoration: InputDecoration(hintText: "Buscar"),
+                ),
+                Expanded(
+                  child: Rows(
+                    text: searchController.text,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
 
-            List<Profile> patients = snapshot.data;
-            Widget list;
-            if (patients == null || patients.isEmpty) {
-              list = Text("No hay ningun paciente registrado");
-            } else {
-              list = ListView.builder(
-                  itemCount: patients.length,
-                  itemBuilder: (context, idx) {
-                    var p = patients[idx];
-                    return Card(
-                      child: Padding(
+class Rows extends StatelessWidget {
+  final _profileService = ProfileService();
+
+  final String text;
+
+  Rows({Key key, this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _profileService.getAll(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState != ConnectionState.done) {
+          return Center(child: CircularProgressIndicator());
+        }
+
+        List<Profile> patients = snapshot.data;
+        if (patients == null || patients.isEmpty) {
+          return Text("No hay ningun paciente registrado");
+        }
+
+        if (text.isNotEmpty) {
+          var t = text.toLowerCase();
+          patients = patients
+              .where((p) =>
+                  p.fullname().toLowerCase().contains(t) ||
+                  p.email.contains(t) ||
+                  p.phone.contains(t))
+              .toList();
+        }
+
+        return ListView.builder(
+            itemCount: patients.length,
+            itemBuilder: (context, idx) {
+              var p = patients[idx];
+              return Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    children: [
+                      Padding(
                         padding: const EdgeInsets.all(10.0),
-                        child: Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: CircleAvatar(
-                                child: Icon(Icons.person),
-                              ),
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  p.fullname(),
-                                ),
-                                Text(p.email),
-                                Text(p.phone),
-                                Text(p.formattedBirthDate())
-                              ],
-                            )
-                          ],
+                        child: CircleAvatar(
+                          child: Icon(Icons.person),
                         ),
                       ),
-                    );
-                  });
-            }
-
-            return Center(
-              child: SizedBox(width: 300, child: list),
-            );
-          },
-        ));
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            p.fullname(),
+                          ),
+                          Text(p.email),
+                          Text(p.phone),
+                          Text(p.formattedBirthDate())
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              );
+            });
+      },
+    );
   }
 }
